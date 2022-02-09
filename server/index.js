c8230ea54b29c288ed00cafb527f110711a1d386
Ben Petersen <bdpetersen77@gmail.com>
2022-02-08 20:37:56 -0600

    test 4

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.get('/test4', (req, res) => {
-    res.send({hi: 'test4'});
+app.get('/test5', (req, res) => {
+    res.send({hi: 'test5'});
 });
 
 app.listen(5000);
