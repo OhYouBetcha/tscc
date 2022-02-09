3245114b4cc629faccf5ba8d0db0a9b0a327f237
Ben Petersen <bdpetersen77@gmail.com>
2022-02-08 21:33:28 -0600

    alot of changes]

--- server/index.js
+++ server/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
-app.get('/10', (req, res) => {
-    res.send({hi: 'there'});
-});
 
-app.get('/test9', (req, res) => {
-    res.send({hi: 'test9'});
-});
-
-app.listen(5000);
+app.listen(3000);
